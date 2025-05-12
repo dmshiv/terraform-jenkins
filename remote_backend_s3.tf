2f982c70a80eb10c1947bc59d688ae4ade26ed9e
dmshiv <shiv6303214@GMAIL.COM>
2025-05-12 18:57:42 +0530

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devops-bucket9898"
-    key    = "devops-project-1/jenkins/terraform.tfstate"
-    region = "eu-west-1"
+    bucket = "devops-bucket9898"              # New bucket name
+    key    = "devops-project-1/jenkins/terraform.tfstate"  # Path to the state file
+    region = "us-east-1"                      # Update the region to us-east-1
   }
 }
